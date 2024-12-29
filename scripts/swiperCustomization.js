27daada14d2e5a84bae11ff6d2392be0106ecb20
BakirWorksDev <bakrirbazarkul17@gmail.com>
2024-12-29 21:17:12 +0300

    finished the chapter concept

---
+++ scripts/swiperCustomization.js
@@ -0,0 +1,27 @@
+import Swiper from "swiper";
+import { EffectFade, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiperConfig = {
+  modules: [EffectFade, Navigation, Pagination],
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".custom-next-button",
+    prevEl: ".custom-prev-button",
+  },
+};
+
+const swiperContainer = document.querySelector(".swiper");
+if (swiperContainer) new Swiper(swiperContainer, swiperConfig);
