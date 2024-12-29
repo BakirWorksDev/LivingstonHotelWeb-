b9a55d8ef75f7097027ebb9dc6589484bebe3b39
BakirWorksDev <bakrirbazarkul17@gmail.com>
2024-12-29 22:35:40 +0300

    fix:config.vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "LivingstonHotelWeb", // Указываете имя вашего репозитория
+  base: "/LivingstonHotelWeb", // Указываете имя вашего репозитория
 });
